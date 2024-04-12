3571ea2a84135343863538a748b5a635dedabc16
Damián Clancig <damian@clancig.com.ar>
2024-04-12 17:37:36 -0300

    Se agrega modal para ver la imagen grande.

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,60 +1,36 @@
-'use client'
-import { useSearchParams, usePathname } from 'next/navigation'
-import Link from 'next/link'
 import Image from 'next/image'
 import DisableBodyScroll from './DisableBodyScroll'
 
-export default function Modal() {
-  const searchParams = useSearchParams()
-  const modal = searchParams.get('modal')
-  const certs = searchParams.get('certs')
-  const pathname = usePathname()
-
+export default function Modal({ handleClose, isShowModal, urlImage }: any) {
   return (
     <>
-      {modal && (
+      {isShowModal ? (
         <>
-          <DisableBodyScroll />
-          {/* // Background blur */}
-          <dialog className="left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 backdrop-blur flex justify-center">
-            {/* Dialog */}
-            <div className="bg-zinc-300 m-auto my-5 p-2 rounded-lg shadow fixed  ">
-              {/* Container */}
-              <div className="flex flex-col items-center">
-                {/* Content */}
-                <div className="container mx-auto px-2 py-2 lg:px-2 lg:pt-2 overflow-y-scroll  max-h-[600px] sm:max-h-[720px] lg:max-h-[800px]">
-                  {/* Data flex */}
-                  <div className="-m-1 flex flex-wrap md:-m-2 justify-center">
-                    {certs?.split(',').map((cert, index) => {
-                      return (
-                        <div key={index} className="flex w-1/2 flex-wrap md:w-1/5">
-                          <div className="w-full p-1 drop-shadow-lg md:p-3">
-                            <Image
-                              alt={cert}
-                              className="block h-full w-full rounded-lg object-cover object-center"
-                              src={'./images/certificados/' + cert}
-                              width={50}
-                              height={50}
-                            />
-                          </div>
-                        </div>
-                      )
-                    })}
-                  </div>
-                </div>
-                <br />
-                <Link href={pathname}>
+          {/* <DisableBodyScroll /> */}
+          <div className="bg-black/70 sticky inset-0 h-screen grid">
+            <div className="w-11/12 h-5/6 m-auto">
+              <div className="h-full flex justify-center items-center">
+                <div className="relative w-full max-h-[90vh] aspect-square">
                   <button
-                    type="button"
-                    className="text-xl text-white bg-slate-600 hover:bg-slate-900 rounded-lg px-4 py-1 text-center"
+                    onClick={handleClose}
+                    className="relative w-full max-h-[90vh] aspect-square"
                   >
-                    Cerrar
+                    <Image
+                      title="Cerrar"
+                      alt={urlImage}
+                      src={urlImage}
+                      fill
+                      sizes="500px"
+                      className="object-contain"
+                    />
                   </button>
-                </Link>
+                </div>
               </div>
             </div>
-          </dialog>
+          </div>
         </>
+      ) : (
+        ''
       )}
     </>
   )
--- app/viewcerts/page.tsx
+++ app/viewcerts/page.tsx
@@ -2,38 +2,54 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import Modal from '../components/Modal'
 
 export default function ViewCerts() {
-  const searchParams = useSearchParams()
-  const certs = searchParams.get('certs')?.split(',')
+  const certs = useSearchParams().get('certs')?.split(',')
 
+  const [showModal, setShowModal] = useState(false)
+  const [urlImage, setUrlImage] = useState('')
+
+  const openModalHanlder = (url: string) => {
+    setUrlImage(url)
+    setShowModal(true)
+  }
+
+  const closeModalHanlder = () => {
+    setShowModal(false)
+  }
   return (
-    <div className="w-4/6 max-lg:w-full h-screen max-lg:h-full m-auto">
-      <section className="px-2 py-3 grid gap-2 grid-cols-gallery">
-        {certs?.map((cert, index) => (
-          <div
-            key={'cert_' + index}
-            className="h-40 bg-gray-200 rounded-xl relative overflow-hidden group"
+    <>
+      <div className="w-4/6 max-lg:w-full m-auto">
+        <section className="px-2 py-3 grid gap-2 grid-cols-gallery">
+          {certs?.map((cert, index) => (
+            <div
+              key={'cert_' + index}
+              className="h-40 bg-gray-200 rounded-xl relative overflow-hidden group"
+            >
+              <Image
+                alt={cert}
+                src={'./images/certificados/' + cert}
+                fill={true}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw 33vw"
+                className="object-cover group-hover:opacity-50"
+                onClick={() => openModalHanlder('./images/certificados/' + cert)}
+              />
+            </div>
+          ))}
+          <br />
+        </section>
+        <Link href={'/#courses'} className="flex justify-center pb-5">
+          <button
+            type="button"
+            className="text-xl text-white bg-slate-600 hover:bg-slate-900 rounded-lg px-4 py-1 text-center"
           >
-            <Image
-              alt={cert}
-              src={'./images/certificados/' + cert}
-              fill={true}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw 33vw"
-              className="object-cover group-hover:opacity-50"
-            />
-          </div>
-        ))}
-        <br />
-      </section>
-      <Link href={'/#courses'} className="flex justify-center pb-5">
-        <button
-          type="button"
-          className="text-xl text-white bg-slate-600 hover:bg-slate-900 rounded-lg px-4 py-1 text-center"
-        >
-          Cerrar
-        </button>
-      </Link>
-    </div>
+            Cerrar
+          </button>
+        </Link>
+      </div>
+      <Modal isShowModal={showModal} urlImage={urlImage} handleClose={closeModalHanlder} />
+    </>
   )
 }
